df966eab275f725cba7912bef312c248f591b4c6
rocwong-cn <me@rocwong.cn>
2021-06-02 20:00:36 +0800

    fix: 基础架构调整

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
-import 'virtual:windi.css'
-import 'virtual:windi-devtools'
 import './styles/main.css'
 
 const routes = setupLayouts(generatedRoutes)
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -10,47 +10,26 @@ const go = () => {
     router.push(`/hi/${encodeURIComponent(name.value)}`)
 }
 
-</script>
+</script> -->
 
 <template>
-  <div>
-    <p class="text-4xl">
-      <carbon-campsite class="inline-block" />
-    </p>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em class="text-sm opacity-75">aaaaaa</em>
-    </p>
-
-    <div class="py-4" />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="输入你的名字"
-      type="text"
-      autocomplete="false"
-      class="px-4 py-2 text-sm text-center bg-transparent border border-gray-200 rounded outline-none active:outline-none dark:border-gray-700"
-      style="width: 250px"
-      @keydown.enter="go"
-    >
-    <label class="hidden" for="input">输入你的名字</label>
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-       GO
-      </button>
-    </div>
-  </div>
+  <main>
+    <h1>Hello, Magic CV !</h1>
+    <button class="start">Get Started</button>
+  </main>
 </template>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+h1 {
+  color: var(--prism-foreground);
+}
+</style>
 
 <route lang="yaml">
 meta:
